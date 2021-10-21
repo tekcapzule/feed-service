4f00ead91823269fc46f70c4d53dec9fcab56547
Linjith  Kunnon <linjithkunnon@Linjiths-MacBook-Pro.local>
2021-10-21 08:44:57 +0530

    refactoring

--- domain/src/main/java/com/tekcapsule/capsule/domain/model/Status.java
+++ domain/src/main/java/com/tekcapsule/capsule/domain/model/Status.java
@@ -1,4 +1,9 @@
 package com.tekcapsule.capsule.domain.model;
 
 public enum Status {
+    SUBMITTED,
+    APPROVED,
+    REJECTED,
+    INACTIVE,
+    EXPIRED
 }
--- domain/src/main/java/com/tekcapsule/capsule/domain/service/CapsuleServiceImpl.java
+++ domain/src/main/java/com/tekcapsule/capsule/domain/service/CapsuleServiceImpl.java
@@ -1,7 +1,7 @@
 package com.tekcapsule.capsule.domain.service;
 
 import com.tekcapsule.capsule.domain.command.DisableCommand;
-import com.tekcapsule.capsule.domain.model.Capsule;
+import com.tekcapsule.capsule.domain.model.*;
 import com.tekcapsule.capsule.domain.repository.CapsuleDynamoRepository;
 import com.tekcapsule.capsule.domain.command.CreateCommand;
 import com.tekcapsule.capsule.domain.command.UpdateCommand;
@@ -24,9 +24,28 @@ public class CapsuleServiceImpl implements CapsuleService {
     @Override
     public Capsule create(CreateCommand createCommand) {
 
-        log.info(String.format("Entering create capsule service - Capsule Name:{0}", createCommand.getTitle()));
+        log.info(String.format("Entering create capsule service - Capsule Title:{0}", createCommand.getTitle()));
 
         Capsule capsule = Capsule.builder()
+                .audience(createCommand.getAudience())
+                .author(createCommand.getAuthor())
+                .description(createCommand.getDescription())
+                .topicName(createCommand.getTopicName())
+                .editorsPick(createCommand.isEditorsPick())
+                .imageUrl(createCommand.getImageUrl())
+                .duration(createCommand.getDuration())
+                .publishedDate(createCommand.getPublishedDate())
+                .expiryDate(createCommand.getExpiryDate())
+                .publisher(createCommand.getPublisher())
+                .resourceUrl(createCommand.getResourceUrl())
+                .level(createCommand.getLevel())
+                .status(Status.SUBMITTED)
+                .tags(createCommand.getTags())
+                .title(createCommand.getTitle())
+                .type(createCommand.getType())
+                .views(0)
+                .recommendations(0)
+                .bookmarks(0)
                 .active(true)
                 .build();
 
@@ -40,11 +59,28 @@ public class CapsuleServiceImpl implements CapsuleService {
     @Override
     public Capsule update(UpdateCommand updateCommand) {
 
-        log.info(String.format("Entering update capsule service - Capsule Name:{0}", updateCommand.getTitle()));
+        log.info(String.format("Entering update capsule service - Capsule Title:{0}", updateCommand.getTitle()));
 
         Capsule capsule = capsuleDynamoRepository.findBy(updateCommand.getTitle());
         if (capsule != null) {
             capsule.setActive(true);
+
+            capsule.setAudience(updateCommand.getAudience());
+            capsule.setAuthor(updateCommand.getAuthor());
+            capsule.setDescription(updateCommand.getDescription());
+            capsule.setTopicName(updateCommand.getTopicName());
+            capsule.setPublishedDate(updateCommand.getPublishedDate());
+            capsule.setTitle(updateCommand.getTitle());
+            capsule.setImageUrl(updateCommand.getImageUrl());
+            capsule.setDuration(updateCommand.getDuration());
+            capsule.setAuthor(updateCommand.getAuthor());
+            capsule.setTags(updateCommand.getTags());
+            capsule.setPublisher(updateCommand.getPublisher());
+            capsule.setResourceUrl(updateCommand.getResourceUrl());
+            capsule.setType(updateCommand.getType());
+            capsule.setAudience(updateCommand.getAudience());
+            capsule.setExpiryDate(updateCommand.getExpiryDate());
+            capsule.setLevel(updateCommand.getLevel());
 
             capsule.setUpdatedOn(updateCommand.getExecOn());
             capsule.setUpdatedBy(updateCommand.getExecBy().getUserId());
@@ -52,12 +88,13 @@ public class CapsuleServiceImpl implements CapsuleService {
             capsuleDynamoRepository.save(capsule);
         }
         return capsule;
+
     }
 
     @Override
     public void disable(DisableCommand disableCommand) {
 
-        log.info(String.format("Entering disable capsule service -  Capsule Name:{0}", disableCommand.getTopicName()));
+        log.info(String.format("Entering disable capsule service -  Capsule Title:{0}", disableCommand.getTopicName()));
 
         Capsule capsule = capsuleDynamoRepository.findBy(disableCommand.getTopicName(),disableCommand.getPublishedDate());
         if (capsule != null) {
