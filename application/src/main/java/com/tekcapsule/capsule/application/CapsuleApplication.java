d3af0a01d263bf56f10638ca4b9c83c214a6fb0e
Linjith  Kunnon <linjithkunnon@Linjiths-MacBook-Pro.local>
2021-11-04 10:09:46 +0530

    dynamodb repo scanning issue fix

--- application/src/main/java/com/tekcapsule/capsule/application/CapsuleApplication.java
+++ application/src/main/java/com/tekcapsule/capsule/application/CapsuleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.tekcapsule.capsule","com.tekcapsule.core.config"})
-@EnableDynamoDBRepositories(dynamoDBMapperConfigRef = "getDynamoDBMapperConfig", basePackages = "com.tekcapsule.core.config")
+@EnableDynamoDBRepositories(dynamoDBMapperConfigRef = "getDynamoDBMapperConfig", basePackages ={ "com.tekcapsule.core.config","com.tekcapsule.capsule.domain.repository"})
 public class CapsuleApplication {
 
     public static void main(String[] args) {
