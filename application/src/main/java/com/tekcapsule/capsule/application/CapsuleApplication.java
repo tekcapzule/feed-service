2ad11f86c30d7afedb8768e3468d52fcf2d82c7d
Linjith  Kunnon <linjithkunnon@Linjiths-MacBook-Pro.local>
2021-11-04 11:11:58 +0530

    refactoring

--- application/src/main/java/com/tekcapsule/capsule/application/CapsuleApplication.java
+++ application/src/main/java/com/tekcapsule/capsule/application/CapsuleApplication.java
@@ -1,11 +1,9 @@
 package com.tekcapsule.capsule.application;
 
-import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.tekcapsule.capsule","com.tekcapsule.core.config"})
-@EnableDynamoDBRepositories(dynamoDBMapperConfigRef = "getDynamoDBMapperConfig", basePackages ={ "com.tekcapsule.core.config","com.tekcapsule.capsule.domain.repository"})
 public class CapsuleApplication {
 
     public static void main(String[] args) {
